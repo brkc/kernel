91697ad76965b480f01a64976c920270c393bdac
Brandon Coffman <brandon.coffman@gmail.com>
2014-09-05 19:37:00 +0000

    Print end address of kernel.

--- kernel.c
+++ kernel.c
@@ -2,12 +2,14 @@
 #include "x86.h"
 
 extern u8 _start[];
+extern u8 end[];
 
 int
 cmain(void)
 {
     cursor_move(0);
     printf("kernel loaded at 0x%x\n", _start);
+    printf("kernel end at 0x%x\n", end);
     printf("ok\n");
     return 0;
 }
