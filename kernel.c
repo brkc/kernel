9815f7bcdab6be72f6a7a15c562e7ec98ec6c998
Brandon Coffman <brandon.coffman@gmail.com>
2014-09-05 19:37:52 +0000

    Print memory map on boot.

--- kernel.c
+++ kernel.c
@@ -1,3 +1,4 @@
+#include "multiboot.h"
 #include "types.h"
 #include "x86.h"
 
@@ -5,11 +6,21 @@ extern u8 _start[];
 extern u8 end[];
 
 int
-cmain(void)
+cmain(u32 magic, multiboot_info_t *info)
 {
+    u32 p;
+
     cursor_move(0);
     printf("kernel loaded at 0x%x\n", _start);
     printf("kernel end at 0x%x\n", end);
+
+    p = info->mmap_addr;
+    while (p < info->mmap_addr + info->mmap_length) {
+        multiboot_memory_map_t *mmap = (multiboot_memory_map_t *) p;
+        u32 size = mmap->size, addr = mmap->addr, len = mmap->len;
+        printf("addr %x len %d\n", addr, len);
+        p += size + sizeof size;
+    }
     printf("ok\n");
     return 0;
 }
