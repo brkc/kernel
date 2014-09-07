a6e1cfa10e60ead79e9d196dfa5e03d25492cb36
Brandon Coffman <brandon.coffman@gmail.com>
2014-09-07 06:10:50 +0000

    Write a quick physical memory allocator.

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include "multiboot.h"
 #include "types.h"
 #include "x86.h"
+#include "vm.h"
 
 extern u8 _start[];
 extern u8 _end[];
@@ -9,6 +10,7 @@ int
 cmain(u32 magic, multiboot_info_t *info)
 {
     u32 p;
+    int i;
 
     cursor_move(0);
     printf("kernel begins at 0x%x\n", _start);
@@ -20,6 +22,11 @@ cmain(u32 magic, multiboot_info_t *info)
         u32 size = mmap->size, addr = mmap->addr, len = mmap->len;
         printf("addr %x len %d\n", addr, len);
         p += size + sizeof size;
+    }
+    physinit(_end, 8 * 1048576);
+    for (i = 0; i < 5; i++) {
+        void *p = physalloc1();
+        printf("allocd addr %x\n", p);
     }
     printf("ok\n");
     return 0;
