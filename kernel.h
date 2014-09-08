c88a6517ec0817f50b165190d96e1a9e3a194536
Brandon Coffman <brandon.coffman@gmail.com>
2014-09-08 00:41:01 +0000

    Declare functions in header file.

--- kernel.h
+++ kernel.h
@@ -3,3 +3,12 @@
 #define PAGE_SIZE 4096
 #define PAGE_ROUNDDOWN(x) ((x) & -(PAGE_SIZE))
 #define PAGE_ROUNDUP(x) PAGE_ROUNDDOWN((x) + ((PAGE_SIZE) - 1))
+
+void cursor_move(u16 offset);
+void kputc(u8 c);
+void kputs(const char *s);
+void kprintf(const char *fmt, ...);
+void kcls(void);
+void physfree(void *addr0);
+void physinit(void *addr0, u32 n);
+void *physalloc1(void);
