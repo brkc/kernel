29069f7dfa8f00cd254c56d1eb7ef966dcc3163a
Brandon Coffman <brandon.coffman@gmail.com>
2014-09-09 19:03:17 +0000

    Add cr0 and cr3 functions.

--- x86.h
+++ x86.h
@@ -2,6 +2,11 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 
+enum {
+    CR0_PE = 1 << 0,
+    CR0_PG = 1 << 31,
+};
+
 static inline u8
 inb(u16 port)
 {
@@ -48,4 +53,36 @@ static inline void
 outl(u16 port, u32 data)
 {
     asm("outl %0,%1" :: "a" (data), "d" (port));
+}
+
+static inline void
+setcr0(u32 cr0)
+{
+    asm("mov %0,%%cr0" :: "a" (cr0));
+}
+
+static inline void
+setcr3(u32 cr3)
+{
+    asm("mov %0,%%cr3" :: "a" (cr3));
+}
+
+static inline u32
+getcr0(void)
+{
+    u32 cr0;
+
+    asm("mov %%cr0,%0" : "=a" (cr0) :);
+
+    return cr0;
+}
+
+static inline u32
+getcr3(void)
+{
+    u32 cr3;
+
+    asm("mov %%cr3,%0" : "=a" (cr3) :);
+
+    return cr3;
 }
